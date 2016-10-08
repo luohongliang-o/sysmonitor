95e80e1977fc85278b1433bd92700d45081f82df
obabywawa <lhbabyblue@163.com>
2016-10-08 17:09:00 +0800

    modify

--- SysMonitor/monitor_system.cpp
+++ SysMonitor/monitor_system.cpp
@@ -344,15 +344,16 @@ CMySqlMonitor::write(int fd, char* buf)
 //////////////////////////////////////////////////////////////////////////
 void CBuildMonitor::ConcreteMonotor(int type, CLoadConfig* loadconfig)
 {
-#ifdef WIN32
+
 	if (MONITORTYPE_SYSTEM_INFO == type)
 		m_system_monitor = new CSysInfo(loadconfig);
 	if (MONITORTYPE_PROCESS == type)
 		m_system_monitor = new CProcessMonitor(loadconfig);
-#endif // WIN32
 
 	if (MONITORTYPE_MYSQL == type)
 		m_system_monitor = new CMySqlMonitor(loadconfig);
+	if (MONITORTYPE_LINUX_SYSINFO == type)
+		m_system_monitor = new CLinuxSysinfo(loadconfig);
 }
 
 CBuildMonitor::~CBuildMonitor()
