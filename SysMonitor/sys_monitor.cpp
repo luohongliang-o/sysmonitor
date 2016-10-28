e35807717723fb71b0aa0aadbb6b85b25912dacc
obabywawa <lhbabyblue@163.com>
2016-10-28 18:21:59 +0800

    add initialize thread when run programe and wipe out old way to initialize timer thread

--- SysMonitor/sys_monitor.cpp
+++ SysMonitor/sys_monitor.cpp
@@ -40,20 +40,25 @@ extern "C" {
 #endif
 #include "func.h"
 #define BUFLEN 1024*4
-
 HANDLE g_time_handle = NULL;
 volatile BOOL g_thread_on_of = TRUE;
+struct packet{
+	long packet_len;
+	char buf[BUFLEN];
+};
 struct client {
 // 	struct event* ev_timer;
 	int fd;
 	struct bufferevent *buf_ev;
 	CLoadConfig*     load_config;
 	CProtocolManage* proto_manage;
+	char*            buffer;
 };
 struct monitor_global 
 {
 	struct event_base *ev_base;
 	CLoadConfig*     load_config;
+	CProtocolManage* proto_manage;
 };
 struct timeval time_val;
 
@@ -69,37 +74,15 @@ struct timeval time_val;
 #define WARN(erromsg) warn(erromsg);
 #endif
 
-int
-setnonblock(int fd)
-{
-	
-#ifndef WIN32
-	int flags;
-	flags = fcntl(fd, F_GETFL);
-	if (flags < 0)
-		return flags;
-	flags |= O_NONBLOCK;
-	if (fcntl(fd, F_SETFL, flags) < 0)
-		return -1;
-#else
-	unsigned long flags;
-	/* ÉèÖÃ·Ç×èÈû */
-	flags = 1;
-	ioctlsocket(fd, FIONBIO, &flags);
-	return WSAGetLastError();
-#endif
-}
-
-
 static unsigned __stdcall
 on_timer(void *arg)
 {
-	client *ins_client = (client*)arg;
-	if (ins_client){
+	monitor_global *g_monitor = (monitor_global*)arg;
+	if (g_monitor){
 		while (g_thread_on_of){
-			if (!ins_client->proto_manage)
-				ins_client->proto_manage = new CProtocolManage(ins_client->load_config);
-			int write_len = ins_client->proto_manage->write(ins_client->fd);
+			if (!g_monitor->proto_manage)
+				g_monitor->proto_manage = new CProtocolManage(g_monitor->load_config);
+			int write_len = g_monitor->proto_manage->write();
 			char current_time[128] = "";
 			strcpy(current_time, GetFormatSystemTime());
 			printf("\nwrite time:%s data len:%d bytes\n", current_time, write_len);
@@ -108,50 +91,28 @@ on_timer(void *arg)
 	}
 	return 0;
 }
-/*
-void
-on_timer(int fd, short event, void *arg)
-{
-	client *ins_client = (client*)arg;
-	if (ins_client){
-		if (!ins_client->proto_manage)
-			ins_client->proto_manage = new CProtocolManage(ins_client->load_config);
-		int write_len = ins_client->proto_manage->write(fd);
-		char current_time[128] = "";
-		strcpy(current_time, GetFormatSystemTime());
-		printf("\nwrite time:%s data len:%d bytes\n", current_time, write_len);
-		time_val.tv_sec = 4;
-		time_val.tv_usec = 0;
-		evtimer_del(ins_client->ev_timer);
-		evtimer_add(ins_client->ev_timer, &time_val);
-	}
-}
-*/
 
 void
 buffered_on_read(struct bufferevent *bev, void *arg)
 {
-	struct client *client = (struct client *)arg;
 	size_t buffer_len = evbuffer_get_length(bev->input);
 	if (buffer_len > 0)
 	{
-		char *buf = NULL;
-		buf = (char*)malloc(BUFLEN);
-		if (buf == NULL)
-			err_plantform(1, " malloc failed");
-		evbuffer_copyout(bev->input, buf, buffer_len);
-
-		if (!client->proto_manage)
-			client->proto_manage = new CProtocolManage(client->load_config);
-		int read_buf_len = client->proto_manage->read(client->fd, buf);
-		char current_time[128] = "";
-		strcpy(current_time, GetFormatSystemTime());
-		printf("\nread time:%s data len:%d bytes\n", current_time, read_buf_len);
-		bufferevent_write(bev, buf, strlen(buf)+1);
-		if (buf){
-			free(buf);
-			buf = NULL;
-		}	
+		struct client *client = (struct client *)arg;
+		evbuffer_remove(bev->input, client->buffer, buffer_len);
+		if (client->proto_manage){
+			packet packet_data;
+			client->buffer[buffer_len] = '\0';
+			int read_buf_len = client->proto_manage->read(client->fd, client->buffer);
+			packet_data.packet_len = read_buf_len + sizeof(packet_data.packet_len);
+			memcpy(packet_data.buf, client->buffer, read_buf_len);
+			if (read_buf_len > 0 && !strstr(client->buffer, "check error.")){
+				char current_time[128] = "";
+				strcpy(current_time, GetFormatSystemTime());
+				printf("\nread time:%s data len:%d bytes\n", current_time, packet_data.packet_len);
+				bufferevent_write(bev, &packet_data, packet_data.packet_len);
+			}
+		}
 	}
 }
 
@@ -176,14 +137,9 @@ buffered_on_error(struct bufferevent *bev, short what, void *arg)
 		WARN("\nClient socket error, disconnecting.\n");
 	}
 	bufferevent_free(client->buf_ev);
-#ifdef WIN32
-	if (WaitForSingleObject(g_time_handle, 500) == WAIT_TIMEOUT);
-		TerminateThread(g_time_handle,0);
-	CLOSEHANDLE(g_time_handle);
-#endif
-
-	TDEL(client->proto_manage);
-	//evtimer_del(client->ev_timer);
+	client->load_config = NULL;
+	client->proto_manage = NULL;
+	TDELARRAY(client->buffer);
 #ifdef WIN32
 	closesocket(client->fd);
 #else
@@ -206,36 +162,24 @@ on_accept(int fd, short ev, void *arg)
 		WARN("accept failed");
 		return;
 	}
-	
-// 	if (setnonblock(client_fd) < 0)
-// 		WARN("failed to set client socket non-blocking");
-
 	ins_client = (client*)calloc(1, sizeof(*ins_client));
 	if (ins_client == NULL)
 		err_plantform(1, "malloc failed");
 	ins_client->load_config = g_monitor->load_config;
-
+	ins_client->proto_manage = g_monitor->proto_manage;
+	ins_client->buffer = new char[BUFLEN];
+	
 	ins_client->fd = client_fd;
-	evutil_make_socket_nonblocking(client_fd);
-/*
-	ins_client->buf_ev = bufferevent_new(client_fd, buffered_on_read,
-		buffered_on_write, buffered_on_error, ins_client);
-*/
 	
 	ins_client->buf_ev = bufferevent_socket_new(g_monitor->ev_base, client_fd, BEV_OPT_CLOSE_ON_FREE);
+
+	evutil_make_socket_nonblocking(client_fd);
+
 	bufferevent_setcb(ins_client->buf_ev, buffered_on_read,
-		buffered_on_write, buffered_on_error, ins_client);
+		NULL, buffered_on_error, ins_client);
 
-	bufferevent_enable(ins_client->buf_ev, EV_READ | EV_WRITE);
+	bufferevent_enable(ins_client->buf_ev, EV_READ |EV_WRITE|EV_PERSIST);
 
-/*
-	ins_client->ev_timer = event_new(g_monitor->ev_base, fd, 0, on_timer, ins_client);
- 	time_val.tv_sec = 1;
- 	time_val.tv_usec = 0;
- 	evtimer_add(ins_client->ev_timer, &time_val);
-*/
-	unsigned threadid;
-	g_time_handle = (HANDLE)_beginthreadex(NULL, 0, on_timer, ins_client, 0, &threadid);
 	printf("Accepted connection from addr:%s port:%d\n",
 		inet_ntoa(client_addr.sin_addr), htons(client_addr.sin_port));
 
@@ -267,12 +211,13 @@ main(int argc, char **argv)
 	listen_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (listen_fd < 0)
 		err_plantform(1, "listen failed");
-	else evutil_make_socket_nonblocking(listen_fd);
+	evutil_make_listen_socket_reuseable(listen_fd);
 
 	monitor_global* g_monitor = new monitor_global;
 	g_monitor->load_config = new CLoadConfig;
 	g_monitor->ev_base = eventbase;
 	CLoadConfig::LoadConfig(g_monitor->load_config);
+	g_monitor->proto_manage = new CProtocolManage(g_monitor->load_config);
 
 	memset(&listen_addr, 0, sizeof(listen_addr));
 	listen_addr.sin_family = AF_INET;
@@ -286,25 +231,27 @@ main(int argc, char **argv)
 	if (listen(listen_fd, 5) < 0)
 		err_plantform(1, "\nlisten failed");
 	reuseaddr_on = 1;
-	setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, (char*)&reuseaddr_on,
-		sizeof(reuseaddr_on));
-
-/*
-	if (setnonblock(listen_fd) < 0)
-		err_plantform(1, "failed to set server socket to non-blocking");
-*/
-
-#ifdef WIN32
-	g_monitor->load_config->get_sys_os_info();
-#endif // WIN32
-
+	evutil_make_socket_nonblocking(listen_fd);
 	event_set(&ev_accept, listen_fd, EV_READ | EV_PERSIST, on_accept, g_monitor);
 	event_add(&ev_accept, NULL);
-
+	unsigned threadid;
+#ifdef WIN32
+	g_time_handle = (HANDLE)_beginthreadex(NULL, 0, on_timer, g_monitor, CREATE_SUSPENDED, &threadid);
+	g_monitor->load_config->get_sys_os_info();
+	ResumeThread(g_time_handle);
+#endif // WIN32
+	
 	event_dispatch();
 	TDEL(g_monitor->load_config);
+	TDEL(g_monitor->proto_manage);
 	event_base_free(g_monitor->ev_base);
 	TDEL(g_monitor);
+
+#ifdef WIN32
+	if (WaitForSingleObject(g_time_handle, 500) == WAIT_TIMEOUT);
+	TerminateThread(g_time_handle, 0);
+	CLOSEHANDLE(g_time_handle);
+#endif
 
 	return 0;
 }
